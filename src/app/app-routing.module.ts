42f85c29e63427e9460f457f0ccd07ac8179ec0a
FernandoMotta <fernandosantosmotta@gmail.com>
2022-10-09 21:20:02 -0300

    adiciona router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { LoginComponent } from './admin/login/login.component';
+import { RegisterComponent } from './admin/register/register.component';
+import { canActivate, redirectLoggedInTo, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
+
+const redirectLoggedInToDashboard = () => redirectLoggedInTo(['dashboard']); 
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, ...canActivate(redirectLoggedInToDashboard) },
+  { path: 'registro', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent, ...canActivate(redirectUnauthorizedToLogin) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
