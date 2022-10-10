09daeb828609181871963bdeaf6027987b777edf
FernandoMotta <fernandosantosmotta@gmail.com>
2022-10-09 21:21:00 -0300

    adiciona chaves de api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'firebase-angular-auth',
+    appId: '1:646693887683:web:a1a7f72aa691527eb04c76',
+    storageBucket: 'firebase-angular-auth.appspot.com',
+    apiKey: 'AIzaSyAv33-IPNm7bKQKkGxAVWpIhFY5y9hA7iE',
+    authDomain: 'fir-angular-auth-f48f0.firebaseapp.com',
+    messagingSenderId: '646693887683',
+  },
   production: false
 };
 
